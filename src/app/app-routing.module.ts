6e99a4d9641d67e32a5bcb513ffee6172354c926
joseMendoza9696 <jmendozacarrasco@gmail.com>
2020-06-24 17:48:55 -0400

    Login de prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,11 @@ const routes: Routes = [
   // { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
 
-  { path: 'home', component: HomeComponent, canActivate: [ AuthGuard ] , children:[
+  // TODO: para produccion utilizar lo que esta comentado
+  // { path: 'home', component: HomeComponent, canActivate: [ AuthGuard ] , children:[
+
+
+  { path: 'home', component: HomeComponent, children:[
       { path: 'usuarios', component: UsuariosComponent },
       { path: 'nuevo-usuario', component: NuevoUsuarioComponent },
       { path: 'usuario-roles', component: UsuarioRolesComponent },
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -16,6 +16,7 @@ export class AuthGuard implements CanActivate {
       return true;
     } else{
       this.router.navigateByUrl('/login');
+      // this.router.navigateByUrl('/home');
       return false;
     }
   }
