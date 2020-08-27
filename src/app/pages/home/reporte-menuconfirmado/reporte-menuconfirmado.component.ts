a7fab60a10a0499e830b60efe6317efab8f59a60
joseMendoza9696 <jmendozacarrasco@gmail.com>
2020-08-27 13:18:09 -0400

    Menu historico confirmado q/ comentarios

--- src/app/pages/home/reporte-menuconfirmado/reporte-menuconfirmado.component.ts
+++ src/app/pages/home/reporte-menuconfirmado/reporte-menuconfirmado.component.ts
@@ -22,51 +22,6 @@ export class ReporteMenuconfirmadoComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // Aprovado: true
-  // AprovadoEba: true
-  // CodigoLiquidoInicial: "2222"
-  // CodigoLiquidoPrimaria: "2222"
-  // CodigoLiquidoSegundaria: "2222"
-  // CodigoSolidoInicial: "1111"
-  // CodigoSolidoPrimaria: "3333"
-  // CodigoSolidoSegundaria: "3333"
-  // EnviadoEba: true
-  // EnviadoJefeUnace: true
-  // Estado: true
-  // FrecuenciaLiquidaUtilizadoInicial: 1.55
-  // FrecuenciaLiquidaUtilizadoPrimaria: 1.11
-  // FrecuenciaLiquidaUtilizadoSegundaria: 1.36
-  // FrecuenciaSolidoUtilizadoInicial: 1.04
-  // FrecuenciaSolidoUtilizadoPrimaria: 11.12
-  // FrecuenciaSolidoUtilizadoSegundaria: 13.61
-  // MontoLiquidaUtilizadoInicial: 8760
-  // MontoLiquidaUtilizadoPrimaria: 8760
-  // MontoLiquidaUtilizadoSegundaria: 8760
-  // MontoSolildoUtilizadaSegundaria: 8759
-  // MontoSolildoUtilizadoInicial: 6490
-  // MontoSolildoUtilizadoPrimaria: 8759
-  // ProductoLiquidoInicial: "Leche deslactosada"
-  // ProductoLiquidoPrimaria: "Leche deslactosada"
-  // ProductoLiquidoSegundaria: "Leche deslactosada"
-  // ProductoSolidoInicial: "Manzana"
-  // ProductoSolidoPrimaria: "Naranja"
-  // ProductoSolidoSegundaria: "Naranja"
-  // codigoGenerado: 2
-  // createdAt: "2020-08-25T16:16:43.950Z"
-  // frecuenciaLiquidaInicialInicial: 0
-  // frecuenciaLiquidaPrimariaInicial: 0
-  // frecuenciaLiquidaSegundariaInicial: 0
-  // frecuenciaSolidoInicialInicial: 0
-  // frecuenciaSolidoPrimariaInicial: 0
-  // frecuenciaSolidoSegundariaInicial: 0
-  // montoLiquidaInicial: 0
-  // montoLiquidaPrimaria: 0
-  // montoLiquidaSegundaria: 0
-  // montoSolidoPrimaria: 0
-  // montoSolildoInicial: 0
-  // montoSolildoSegundaria: 0
-  // updatedAt: "2020-08-25T23:20:49.378Z"
-
   buscarPorFecha(fechaInicio, fechaFinal){
     this.mostrarMenus = true;
     fechaInicio = new Date(fechaInicio.value).toISOString();
