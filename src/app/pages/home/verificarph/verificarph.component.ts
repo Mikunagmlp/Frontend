b1311a0543c72911c3765bf0bf2b9000a9ae2f85
Christian <christiantusco@gmail.com>
2020-09-13 18:10:30 -0400

    registro

---
+++ src/app/pages/home/verificarph/verificarph.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {RegisterphModel} from "../../../models/registerph.model";
+import {UserService} from "../../../services/user.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-verificarph',
+  templateUrl: './verificarph.component.html',
+  styles: [
+  ]
+})
+export class VerificarphComponent implements OnInit {
+
+  verificarph: RegisterphModel = new RegisterphModel();
+
+  constructor( private service: UserService, private router: Router ) { }
+
+  ngOnInit(): void {
+  }
+
+  registrarPh(form: NgForm) {
+    if (form.invalid) { return ; }
+
+    this.service.registrarAlmacen(this.registerph).subscribe(resp => {
+      // console.log(resp);
+      this.router.navigateByUrl('/home/registroph-registrar')
+    });
+  }
+
+  cancelar() {
+      this.router.navigateByUrl('/home/registerph')
+  }
+
+}
